2093ce4302456633636cc30bbc518e9121a44234
avana-muhammadaz <avana-muhammadaz@gcc.gov.sg>
2023-02-06 10:49:40 +0800

    resolve svg issue

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import Image from 'next/image'
 import React, { useState } from 'react'
 import NavHeader from './NavHeader';
 import Sidebar from './Sidebar'
@@ -51,13 +52,14 @@ const Layout = ({ children, topBar }: any) => {
 
           <div className='flex items-center px-4 text-sm lg:p-0'>
             <div className={classNames(buttonWrapper)} onClick={() => clickDropdown(dropdown)}>
-              <button className='pl-[10px] pr-[5px] py-[9px]'>
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M16 2V6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M8 2V6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M3 10H21" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+              <button className='pl-[10px] pr-[5px] py-[9px]'>                
+                <Image
+                  src="/images/icons/calendar.svg"
+                  alt="Close Icon"
+                  className="relative"
+                  width={18}
+                  height={18}
+                />
               </button>
               <div className='w-auto'>
                 This Month
