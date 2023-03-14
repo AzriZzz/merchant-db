14000d0b1f798541cf8003cfb973ae9c8170c973
AzriZzz <lenovorosak@gmail.com>
2023-03-14 16:29:03 +0800

    Fix pulse

--- components/components/Loader.tsx
+++ components/components/Loader.tsx
@@ -1,34 +1,17 @@
 import React from 'react'
 import Image from "next/image";
 
-const Loader = ({ width, height }: { width: number, height: number }) => {
-
-  const pulse = `
-    @keyframes pulse {
-      0% {
-        transform: scale(1);
-      }
-      50% {
-        transform: scale(1.1);
-      }
-      100% {
-        transform: scale(1);
-      }
-    }
-  `;
-
+const Loader = ({ width, height } : any) => {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-white">
-      <style>{pulse}</style>
       <Image
         src="/images/billplz-logo.svg"
         alt="Billplz Logo"
-        className="relative transition-all duration-500 transform animate-pulse"
+        className="relative animate-pulse"
         width={width}
         height={height}
       />
-    </div>
-  );
+    </div>)
 }
 
-export default Loader;
+export default Loader
