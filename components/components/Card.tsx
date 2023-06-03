37d5d5bbeac1fa52d00295789bb6ac54a1b8e679
azrizzz <lenovorosak@gmail.com>
2023-06-03 23:02:45 +0800

    Fix: card header title

--- components/components/Card.tsx
+++ components/components/Card.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import dynamic from 'next/dynamic'
-import { ICard } from '@/models/interface'
-import { formatterDouble } from '@/constants/serviceUtils'
-import LineChart from '../chart/LineChart'
-import Horizontal from './Horizontal'
-import CollectionList from './CollectionList'
+import React from "react";
+import dynamic from "next/dynamic";
+import { ICard } from "@/models/interface";
+import { formatterDouble } from "@/constants/serviceUtils";
+import LineChart from "../chart/LineChart";
+import Horizontal from "./Horizontal";
+import CollectionList from "./CollectionList";
 
 // Turn off rendering during SSR
-const PieChart = dynamic(() => import('../chart/PieChart'), {
+const PieChart = dynamic(() => import("../chart/PieChart"), {
   ssr: false,
-})
+});
 
 const Card = (props: ICard) => {
-
   const { card } = props;
   if (!card) {
     return (
@@ -22,41 +21,75 @@ const Card = (props: ICard) => {
     );
   }
 
+  const {
+    title,
+    buttonTitle,
+    isSimple,
+    isCollapse,
+    total,
+    isCurrency,
+    isPaid,
+    growth,
+    isTrend,
+    line,
+    pie,
+    horizontal,
+    upcoming,
+    chartId,
+  } = card;
+
   return (
-    <div className={`w-full  md:w-[374px] ${card.isSimple ? `h-[93px]` : `h-[312px]`}  bg-neutral-white rounded-lg mt-5 p-5 shadow-card`}>
-      <div className='flex flex-col h-full space-between'>
-        <div className='card card-header'>
-          <div className='flex justify-between space-between'>
-            <div className={`${card.isSimple ? '' : 'font-bold'} `}>
-              <h1>{card.title}</h1>
+    <div
+      className={`w-full  md:w-[374px] ${
+        isSimple ? `h-[93px]` : `h-[312px]`
+      }  bg-neutral-white rounded-lg mt-5 p-5 shadow-card`}
+    >
+      <div className="flex flex-col h-full space-between">
+        <div className="card card-header">
+          <div className="flex justify-between space-between">
+            <div className={`${isSimple ? "" : "font-bold"} `}>
+              <h1>{title}</h1>
             </div>
-            <div className='font-bold cursor-pointer text-primary-blue '>
-              <span>{card.isCollapse ? card.buttonTitle : null}</span>
+            <div className="font-bold cursor-pointer text-primary-blue ">
+              <span>{isCollapse ? buttonTitle : null}</span>
             </div>
           </div>
           <div className={`flex flex-row mt-2.5 `}>
-            <div className={`font-semibold pr-[5px] ${ card.isCurrency && card.isPaid ? 'text-primary-success text-xl -translate-y-[5px]' :
-                card.isCurrency ? 'text-2xl' : 'text-xl -translate-y-[5px]'} `}>
+            <div
+              className={`font-semibold pr-[5px]               
+                ${isPaid && "text-primary-success"}
+                ${isSimple ? "text-xl" : "text-2xl"}
+              `}
+            >
               <span>
-                {card.isCurrency ? formatterDouble.format(Number(card.total)) : card.total}
+                {isCurrency ? formatterDouble.format(Number(total)) : total}
               </span>
             </div>
-            { card.growth !== undefined &&
-              <div className={`text-xl translate-y-[2px] ${card.isTrend ? 'text-primary-success' : 'text-primary-danger'}`}>
-                <span>{card.isTrend ? '\u2191' : '\u2193'}{card.growth}%</span>
+            {growth !== undefined && (
+              <div
+                className={`text-xl translate-y-[2px] ${
+                  isTrend ? "text-primary-success" : "text-primary-danger"
+                }`}
+              >
+                <span>
+                  {isTrend ? "\u2191" : "\u2193"}
+                  {growth}%
+                </span>
               </div>
-            }
+            )}
           </div>
         </div>
-        <div className='card card-body'>
-          {card.line !== undefined && <LineChart dataset={card.line} id={card.chartId} />}
-          {card.pie !== undefined && <PieChart dataset={card.pie} id={card.chartId} />}
-          {card.horizontal !== undefined && <Horizontal dataset={card.horizontal} id={card.chartId} />}
-          {card.upcoming !== undefined && <CollectionList dataset={card.upcoming} />}
+        <div className="card card-body">
+          {line !== undefined && <LineChart dataset={line} id={chartId} />}
+          {pie !== undefined && <PieChart dataset={pie} id={chartId} />}
+          {horizontal !== undefined && (
+            <Horizontal dataset={horizontal} id={chartId} />
+          )}
+          {upcoming !== undefined && <CollectionList dataset={upcoming} />}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card  
+export default Card;
